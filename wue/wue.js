4ffbee0b0091f3b4c555f1003d79094b63830c61
wfyweb <webwfy@163.com>
2021-07-13 18:01:23 +0800

    feat: add compile

--- wue/wue.js
+++ wue/wue.js
@@ -9,6 +9,8 @@ class Wue{
     // 2.5 对vue实例的数据做代理
     this.proxy(this, '$data')
     // 3. compile编译
+    new compile(options.el, this)
+
   }
   proxy(vm, key) {
     const $data = vm[key]
@@ -26,3 +28,52 @@ class Wue{
     }) 
   }
 }
+class Compile{
+  constructor(el, vm) {
+    this.$vm = vm
+    
+    this.compile(document.querySelector(el))
+  }
+  // 遍历解析dom
+  compile(el) {
+    Array.from(el.childNodes).forEach(node=>{
+      if(node.nodeType === 1){
+        // 元素
+        this.compileElement(node)
+      }
+      if(this.isInter(node)){
+        // 插值文本
+        this.compileText(node);
+      }
+    })
+  }
+  // 编译Element
+  compileElement(node) {
+    // 1.获取当前元素的所有属性，并判断他们是不是动态的
+
+    const nodeAttrs = node.attributes
+    Array.from(nodeAttrs).forEach(attr=>{
+      const attrName = attr.name
+      const exp = attr.value
+      // 判断attrName是否是指令或动态件等
+      if(attrName.startsWith('w-')) {
+        const dir = attrName.substring(2)
+        this[dir] && this[dir](node, exp)
+      }
+
+    })
+  }
+  text (node, exp){
+    node.textCenter = this.$vm[exp]
+  }
+  html (node, exp) {
+    node.innerHTML = this.$vm[exp]
+  }
+  isInter(node) {
+    return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textCentent);
+  }
+  compileText(node) {
+    node.textCenter = this.$vm[RegExp.$1]
+  }
+}
+
